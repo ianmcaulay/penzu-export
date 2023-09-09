34784187c57b3c4f9e055a7202aa60a73d1d96ca
ianmcaulay <mcaulay.ian@gmail.com>
2023-09-09 14:41:45 -0500

    Update selenium api to newer version

--- export_penzu_data.py
+++ export_penzu_data.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 
 
 # All sleep times are in seconds.
@@ -52,12 +54,13 @@ def get_url(driver, url):
 
 
 def wait_for_page_load(driver):
-    loader = driver.find_element_by_class_name('global-loader__wrap')
+    loader = driver.find_element(By.CLASS_NAME, 'global-loader__wrap')
     finished_loader_classes = {'global-loader__wrap', 'ng-hide'}
+    time.sleep(5)
     t0 = time.time()
     while time.time() - t0 < MAX_TIMEOUT and set(loader.get_attribute('class').split(' ')) != finished_loader_classes:
         time.sleep(.1)
-        loader = driver.find_element_by_class_name('global-loader__wrap')
+        loader = driver.find_element(By.CLASS_NAME, 'global-loader__wrap')
     time.sleep(SLEEP_AFTER_PAGE_LOAD)
 
 
@@ -66,7 +69,8 @@ def get_driver(headless):
     if headless:
         options.add_argument("--headless")
     options.add_argument("--disable-extensions")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
     return driver
 
 
@@ -91,13 +95,13 @@ def login(driver, email, password, manual_login):
     if manual_login:
         input('Please login and press enter...')
     else:
-        email_input = driver.find_element_by_id('email')
+        email_input = driver.find_element(By.ID, 'email')
         email_input.clear()
         email_input.send_keys(email)
-        password_input = driver.find_element_by_id('password')
+        password_input = driver.find_element(By.ID, 'password')
         password_input.clear()
         password_input.send_keys(password)
-        driver.find_element_by_class_name('js-login-submit').click()
+        driver.find_element(By.CLASS_NAME, 'js-login-submit').click()
         wait_for_page_load(driver)
         time.sleep(SLEEP_AFTER_LOGIN)
 
@@ -105,11 +109,11 @@ def login(driver, email, password, manual_login):
 def get_entries_from_entries_url(driver, entries_url):
     get_url(driver, entries_url)
     entries = []
-    for entry_element in driver.find_elements_by_class_name('entries-list__item'):
-        entry_title_element = singleton(entry_element.find_elements_by_css_selector('.title.item__cell'))
-        entry_url_element = singleton(entry_title_element.find_elements_by_tag_name('a'))
+    for entry_element in driver.find_elements(By.CLASS_NAME, 'entries-list__item'):
+        entry_title_element = singleton(entry_element.find_elements(By.CSS_SELECTOR, '.title.item__cell'))
+        entry_url_element = singleton(entry_title_element.find_elements(By.TAG_NAME, 'a'))
         entry_url = entry_url_element.get_attribute('href')
-        created_at_element = singleton(entry_element.find_elements_by_css_selector('.date.item__cell'))
+        created_at_element = singleton(entry_element.find_elements(By.CSS_SELECTOR, '.date.item__cell'))
         created_at = created_at_element.text
         entries.append(Entry(entry_url, created_at))
 
@@ -144,9 +148,9 @@ def get_all_entries(driver, journal_id):
 
 def get_entry_data(driver, entry):
     get_url(driver, entry.entry_url)
-    text_element = singleton(driver.find_elements_by_class_name('cke_inner'))
+    text_element = singleton(driver.find_elements(By.CLASS_NAME, 'cke_inner'))
     text = text_element.text
-    title_element = singleton(driver.find_elements_by_class_name('h1'))
+    title_element = singleton(driver.find_elements(By.CLASS_NAME, 'h1'))
     title = title_element.get_property('value')
 
     entry_data = {
@@ -170,6 +174,7 @@ def save_entry_data(df, entry_data):
 
 def get_all_entries_data(journal_id, email, password, headless, manual_login):
     df = get_entries_df()
+    driver = None
     try:
         driver = get_driver(headless)
         login(driver, email, password, manual_login)
@@ -182,7 +187,8 @@ def get_all_entries_data(journal_id, email, password, headless, manual_login):
                 df = save_entry_data(df, entry_data)
         print(f'Total number of entries: {len(df)}')
     finally:
-        driver.quit()
+        if driver:
+            driver.quit()
 
 
 if __name__ == '__main__':
